08a262a98f58368fbe10d0a75858f612ad01d14f
Cansur <tjswo0682@gmail.com>
2025-03-13 21:37:18 +0900

    메인 대시보드 디자인 변경

--- Helper_BackEnd/src/main/java/com/example/Helper/controller/MainController.java
+++ Helper_BackEnd/src/main/java/com/example/Helper/controller/MainController.java
@@ -2,14 +2,22 @@ package com.example.Helper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class MainController {
     
     @GetMapping("/")
-    public String MainPage() {
+    public String IndexPage() {
         return "index.html";
     }
+
+    @GetMapping("/main")
+    public String MainPage() {
+        return "/dist/index.html";
+    }
+    
     
 }
